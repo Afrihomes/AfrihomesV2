4a78bbca2dfffbefde082a353ff9cff680a4f98d
Benji <benjamin0000lopez@gmail.com>
2024-08-12 12:38:14 -0700

    changes

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,8 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -48,9 +49,13 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: env.FB_CLIENT_ID,
+      clientSecret: env.FB_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
